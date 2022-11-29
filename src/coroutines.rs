7d558c971fcb2f16536d3501e29cd6bfd3c2a1e7
Maciej Jur <maciej@kamoshi.org>
2022-11-29 15:44:34 +0100

    Fix accidental blocking in asyncio thread

--- src/coroutines.rs
+++ src/coroutines.rs
@@ -1,9 +1,9 @@
 use std::thread;
-use std::time::Duration;
 use chrono::{Datelike, Timelike};
 use serenity::http::Http;
 use serenity::model::prelude::ChannelId;
 use serenity::prelude::Context;
+use tokio::time::{sleep, Duration};
 
 
 pub fn init(ctx: Context) {
@@ -11,13 +11,9 @@ pub fn init(ctx: Context) {
         let channel = ChannelId(841002228964786197);
         let channel = ChannelId(1047123653847957544);
 
-        channel.send_message(&ctx.http, |m| m.content("test"))
-            .await
-            .expect("TODO: panic message");
-
         let mut last_sent = 0_u32;
         loop {
-            thread::sleep(Duration::from_secs(10));
+            sleep(Duration::from_secs(10)).await;
             papal(&ctx.http, &channel, &mut last_sent).await;
         }
     });
@@ -27,7 +23,7 @@ async fn papal(http: &Http, channel: &ChannelId, last_sent: &mut u32) {
     let now = chrono::Local::now();
     let (d, h, m) = (now.day(), now.hour(), now.minute());
 
-    if d != *last_sent && h == 21 && m == 37 {
+    if d != *last_sent && h == 15 && m == 44 {
         channel.send_message(&http, |m| m.content("papież"))
             .await
             .expect("TODO: panic message");
